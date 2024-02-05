e916e7c43b06da75dc3c3f02c5ed84c2eb7df1a7
Spakelios <73300254+Spakelios@users.noreply.github.com>
2024-02-05 17:25:48 +0000

    level select

---
+++ DetectiveHawkshaw/Assets/LevelSelect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void newlevel ()
+    {
+        SceneManager.LoadScene("DemoTest1");
+    }
+    
+    public void newlevel2 ()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+    
+    public void newlevel3()
+    {
+        SceneManager.LoadScene("Coming soon");
+    }
+}
